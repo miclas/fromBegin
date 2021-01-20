bc5566d543b6a369fc86e565858f7cd0cf0cb1b9
Michał Łaski <michal.r.laski@gmail.com>
2021-01-20 13:50:59 +0100

    replace_first

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from typing import Iterable
+
+
 def backward_string(val: str) -> str:
     if not val:
         return ''
-
     a = []
     for x in range(1, len(val)+1):
         a.append(val[len(val) - x])
@@ -9,9 +11,17 @@ def backward_string(val: str) -> str:
     return b
 
 
+def replace_first(items: list) -> Iterable:
+    if items:
+        items.append(items.pop(0))
+    return items
+
+
 if __name__ == '__main__':
     print("Example:")
     print(backward_string('val'))
+    print(list(replace_first([1, 2, 3, 4])))
+
 
     assert backward_string('val') == 'lav'
     assert backward_string('') == ''
