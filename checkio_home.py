160f15dd31c1e50b2252b1810f93a7185beaf653
Michał Łaski <michal.r.laski@gmail.com>
2021-01-27 15:49:44 +0100

    threeWords

--- checkio_home.py
+++ checkio_home.py
@@ -8,6 +8,20 @@ def checkio(array: list) -> int:
     return sum * array[-1]
 
 
+def threeWords(words: str) -> bool:
+    x = words.split()
+    suma = 0
+    for a in x:
+        if a.isdigit():
+            suma = 0
+        else:
+            suma += 1
+            if suma == 3:
+                return True
+    return False
+
+
 if __name__ == '__main__':
     print('Example:')
     print(checkio([0, 1, 2, 3, 4, 5]))
+    print(threeWords("Hello World hello"))
