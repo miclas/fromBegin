02a63aef782f8e1cb3853aac3b8ed4ff0a27511d
Michał Łaski <michal.r.laski@gmail.com>
2021-03-15 20:48:44 +0100

    sum_light

--- o'reilly.py
+++ o'reilly.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from typing import List
+
+
 def replace_last(line: list) -> list:
     if line:
         line.insert(0,line.pop(-1))
@@ -20,7 +24,21 @@ def is_majority(items: list) -> bool:
     return False
 
 
+def sum_light(els: List[datetime]) -> int:
+    result = 0
+    while els:
+        timedelta = els.pop(1) - els.pop(0)
+        result += int(timedelta.total_seconds())
+    return result
+
+
 if __name__ == '__main__':
     print(replace_last([2, 3, 4, 1]))
     print(index_power([1, 2, 3, 4], 2))
     print(is_majority([True, True, False, True, False]))
+    print(sum_light([
+        datetime(2015, 1, 12, 10, 0 , 0),
+        datetime(2015, 1, 12, 10, 10 , 10),
+        datetime(2015, 1, 12, 11, 0 , 0),
+        datetime(2015, 1, 12, 11, 10 , 10),
+    ]))
