ac2e5686271a6a02847115d8e4dad7c07e61722c
Michał Łaski <michal.laski@nokia.com>
2022-03-30 21:12:51 +0200

    find_in

--- scientific_expedition.py
+++ scientific_expedition.py
@@ -31,8 +31,40 @@ def goes_after(word, first, second) -> bool:
         return False
 
 
+def find_in(text, word):
+    splited = text.split('\n')
+    x = []
+    for i, v in enumerate(splited):
+        splited[i] = v.replace(' ', '').lower()
+    for i, v in enumerate(splited):
+        x.append(len(v))
+        if word in v:
+            print(i + 1, v.index(word[0]) + 1, i + 1, v.index(word[0]) + len(word))
+            return [i + 1, v.index(word[0]) + 1, i + 1, v.index(word[0]) + len(word)]
+
+    for a in range(max(x)):
+        kolumna = []
+        for ind, val in enumerate(splited):
+            try:
+                kolumna.append(splited[ind][a])
+            except:
+                continue
+            join = ''.join(kolumna)
+            if word in join:
+                return [join.index(word[0]) + 1, a + 1, join.index(word[0]) + len(word), a + 1]
+    return False
+
+
 if __name__ == '__main__':
     print("Example:")
     print(sort_abs([-20, -5, 10, 15]))
     print(rmAccent("àèìǹòùẁỳÀÈÌǸÒÙẀỲ"))
     print(goes_after("world", "w", "o"))
+    print(find_in("""He took his vorpal sword in hand:
+                    Long time the manxome foe he sought--
+                    So rested he by the Tumtum tree,
+                    And stood awhile in thought.
+                    And as in uffish thought he stood,
+                    The Jabberwock, with eyes of flame,
+                    Came whiffling through the tulgey wood,
+                    And burbled as it came!""", "noir"))
